95502b69494384c24e5ddf61ea7854f38695ae7f
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-05-27 00:00:24 -0500

    Navegacion entre pantallas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.android)
+    implementation(libs.androidx.navigation.compose.jvmstubs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
